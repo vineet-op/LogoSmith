890d767d52a262fc933f92b5e3971b74d5cd53cc
Vineet Jadhav <vineet290403@gmail.com>
2025-01-13 20:46:44 +0530

    Added hugging-face model

--- src/app/api/ai-logo-model/route.jsx
+++ src/app/api/ai-logo-model/route.jsx
@@ -1,15 +1,40 @@
 import { NextResponse } from "next/server"
 import { AiLogoPrompt } from "../../Configs/AiModel"
+import axios from "axios"
 
 export async function POST(req) {
     const { prompt } = await req.json()
 
     try {
         const AiPromptResult = await AiLogoPrompt.sendMessage(prompt)
-        return NextResponse.json(JSON.parse(AiPromptResult.response.text()).prompt)
+        const output = JSON.parse(AiPromptResult.response.text()).prompt
+        console.log("output", output);
+
+        const response = await axios.post(
+            "https://api-inference.huggingface.co/models/strangerzonehf/Flux-Midjourney-Mix2-LoRA",
+            output,
+            {
+                headers: {
+                    Authorization: "Bearer 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",
+                    "Content-Type": "application/json",
+                },
+                responseType: "arraybuffer",
+            }
+        );
+        console.log("data from api", response.data);
+
+        const buffer = Buffer.from(response.data, "binary")
+        const base64Image = buffer.toString("base64")
+
+        const base64ImageWithMime = `data:image/png;base64,${base64Image}`;
+        console.log(base64ImageWithMime);
+
+
+        return NextResponse.json({ image: base64ImageWithMime });
+
     } catch (error) {
-        return NextResponse({
-            error: error
-        })
+        return NextResponse.json({
+            error: error.message
+        });
     }
 }
