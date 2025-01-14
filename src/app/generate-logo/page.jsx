d0a1e9103ba04b36c916c7664482041aac93959e
Vineet Jadhav <vineet290403@gmail.com>
2025-01-14 21:56:09 +0530

    added download button

--- src/app/generate-logo/page.jsx
+++ src/app/generate-logo/page.jsx
@@ -7,8 +7,8 @@ import { Button } from '@/components/ui/button';
 
 const GenerateLogo = () => {
     const [storageData, setStorageData] = useState(null);
-    const [base64Image, setBase64Image] = useState()
-    const [loading, setloading] = useState(false)
+    const [base64Image, setBase64Image] = useState();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         // Retrieve the stored data from localStorage
@@ -41,36 +41,42 @@ const GenerateLogo = () => {
             .replace('{logoDesign}', storageData?.idea || "")
             .replace('{logoPrompt}', storageData?.design?.prompt || "");
 
-        // console.log("Real Prompt", PROMPT);
-
         try {
-            setloading(true)
+            setLoading(true);
             const PromptResponse = await axios.post('/api/ai-logo-model', {
                 prompt: PROMPT
-            })
-            setBase64Image(PromptResponse.data.image)
-            setloading(false)
+            });
+            setBase64Image(PromptResponse.data.image);
+            setLoading(false);
         }
         catch (error) {
             console.error("Error fetching the image:", error);
         }
     };
 
+    const downloadImage = () => {
+        const link = document.createElement('a');
+        link.href = base64Image;
+        link.download = 'AI_generated_logo.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className='flex justify-center items-center flex-col'>
             {
                 loading ? (
-                    /* From Uiverse.io by themrsami */
-                    <div class="flex justify-center items-center h-full">
-                        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-purple-500"></div>
-                        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-red-500 ml-3"></div>
-                        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-blue-500 ml-3"></div>
+                    <div className="flex justify-center items-center h-full">
+                        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-purple-500"></div>
+                        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-red-500 ml-3"></div>
+                        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-blue-500 ml-3"></div>
                     </div>
-
                 ) : (
-                    <div className='rounded-lg w-96 h-96 mt-5'>
-                        <h1 className='mb-10 font-mono text-xl p-3 text-center rounded-lg font-medium text-purple-500 bg-black'>Here is your logo by AI</h1>
+                    <div className='rounded-lg w-96 h-96 flex flex-col gap-2'>
+                        <h1 className='font-mono text-xl text-center rounded-lg font-bold text-purple-500'>Here is your logo by AI</h1>
                         <img src={base64Image} alt={"Logo Image"} className='rounded-lg w-[100%]' />
+                        <Button onClick={downloadImage} className="mt-4 font-mono">Download Logo</Button>
                     </div>
                 )
             }
